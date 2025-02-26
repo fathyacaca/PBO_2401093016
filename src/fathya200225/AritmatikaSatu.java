6efdf5041f69464d746c6f896b3df326b86f825b
WINDOWS <WINDOWS@FATHYA-PC>
2025-02-26 12:32:01 +0700

    TUGAS FATHYA

---
+++ src/fathya200225/AritmatikaSatu.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fathya200225;
+
+/**
+ *
+ * @author LABP1KOMP
+ */
+public class AritmatikaSatu {
+    public static void main(String[] args){
+        //sedikit angka
+        int i = 37;
+        int j =  42;
+        double x = 27.475;
+        double y = 7.22;
+        System.out.println("Variable values...");
+        System.out.println(" i = " + i);
+        System.out.println(" j = " + j);
+        System.out.println(" x = " + x);
+        System.out.println(" y = " + y);
+        //penjumlahan angka 
+        System.out.println("Adding...");
+        System.out.println(" i + j = " + (i + j));
+        System.out.println(" x + y = " + (x + y));
+        //pengurangan angka 
+        System.out.println("Subtracting...");
+        System.out.println(" i - j = " + (i - j));
+        System.out.println(" x - y = " + (x - y));
+    }
+    
+}
---
+++ src/fathya200225/VariablSatu.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fathya200225;
+
+/**
+ *
+ * @author LABP1KOMP
+ */
+public class VariablSatu {
+    public static void main(String[] args){
+        //dekalasi a tipe data dengan nama variable
+        // result dan tipe data boolean
+        boolean result;
+    
+        
+        //deklarasi tipe data dengan nama variable
+        // option dan tipe data char
+        char option;
+        option = 'C';   //menandai 'C' sebagai option
+        System.out.println(option);
+        
+        //deklarasi tipe data dengan nama variable 
+        //grade, double tipe data dan telah di inisialisasi
+        //to 0.0
+        double grade = 0.0;
+        System.out.println(grade);
+    }
+}
